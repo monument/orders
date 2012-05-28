3819498b8df135a3a2cc1ca55710ced44de175fd
Hawken Rives <hawkrives@gmail.com>
2012-05-27 19:59:35 -0500

    edited my `f` function
    Now uses document.querySelectorAll instead of if-else statements.

--- form/form.js
+++ form/form.js
@@ -1,10 +1,5 @@
 function f(element) {
-    if (element.substring(0,1) === '#')
-        return document.getElementById(element.substring(1));
-    else if (element.substring(0,1) === '.')
-        return document.getElementsByClassName(element.substring(1));
-    else if (element.substring(0,1) === "'")
-        return document.getElementsByTagName(element.substring(1,element.length-1));
+    return document.querySelectorAll(element)[0];
 }
 
 function calc() {
@@ -36,7 +31,7 @@ function calc() {
 }
 
 var order = {
-    "order": {
+    "details": {
         "date": "5-23-2012", 
         "by": "Kyle MacKay Rives", 
         "at": "Benchmark", 
@@ -44,126 +39,13 @@ var order = {
     },
     "name": {
         "full": "Victoria M. Charging Hawk", 
+        "sort": "hawk_victoria_m",
         "first": "Victoria", 
         "middle": "M.", 
         "last": "Charging Hawk", 
         "prefix": "", 
         "suffix": "" 
     },
-    "materials": {
-        "piece": {
-            "material": "granite", 
-            "color": "gray", 
-            "type": "tablet", 
-            "quantity": 1, 
-            "length": 24, 
-            "width": 6, 
-            "height": 16, 
-            "shape": "serp-top", 
-            "finish": "pol2", 
-            "size": "single" 
-        },
-        "piece": {
-            "material": "granite",
-            "color": "gray",
-            "type": "base",
-            "quantity": 1,
-            "length": 36,
-            "width": 12,
-            "height": 6,
-            "margin": ""
-        },
-        "piece": {
-            "material": "granite",
-            "color": "gray",
-            "type": "vase",
-            "quantity": 2,
-            "length": 4,
-            "width": 4,
-            "height": 9,
-            "shape": "square", 
-            "finish": "pol2"
-        },
-        "piece": {
-            "material": "granite",
-            "color": "gray",
-            "type": "slant",
-            "quantity": 1,
-            "length": 20,
-            "width": 10,
-            "height": 16,
-            "finish": "face"
-        },
-        "foundation": {
-            "deep": true, 
-            "depth": 18, 
-            "pad": "granite", 
-            "thickness": 3 
-        }
-    },
-    "design": {
-        "preview": "./preview.svg",
-        "title": "Wild roses in an oval panel.",
-        "carving": "flat", 
-        "artwork": "",
-        "family-name": "",
-        "match": false,
-        "face": {
-            "direction": "east", 
-            "on-left": "",
-            "verified": ""
-        }
-    },
-    "setting": {
-        "cemetery": "", 
-        "pick-up": true 
-    },
-    "signature": {
-        "image": "./signature.png",
-        "name": "Roger Whipple",
-        "address": {
-            "street": "4811 S. Jackson Ave, Apt. 33",
-            "city": "Tulsa", 
-            "state": "OK", 
-            "zip": 74107, 
-            "phone": 9188925544 
-        }
-    },
-    "status": {
-        "date": {
-            "written": "5-24-2012",
-            "special": "ASAP",
-            "ordered": ""
-        },
-        "drawing": {
-            "completed": "5-24-2012",
-            "sent": "",
-            "approved": "",
-            "production": ""
-        },
-        "bronze": {
-            "ordered": "",
-            "recieved": ""
-        },
-        "vases": {
-            "ordered": "",
-            "recieved": ""
-        },
-        "stone": {
-            "ordered": "",
-            "recieved": "",
-            "production": "",
-            "office": "",
-            "contacted": "",
-            "approved": ""
-        },
-        "setting": {
-            "fee": "",
-            "installed": "",
-            "notified": "",
-            "via": "" 
-        }
-    }
 }
 
 function nameMerge() {
