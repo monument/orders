3bbc1538bed37e0c031aa3a835a2e89bc0addc09
Hawken Rives <hawkrives@gmail.com>
2015-04-05 23:56:57 -0500

    Save data to localStorage

--- index.js
+++ index.js
@@ -3,8 +3,23 @@ import React from 'react'
 import App from './js/app'
 import OrderFlux from './flux/OrderFlux'
 
+import throttle from 'lodash/function/throttle'
+
 let Flux = new OrderFlux()
-Flux.getActions('orders').createOrder({})
-window.acts = Flux.getActions('orders')
+window.f = Flux
+
+const oldData = localStorage.getItem('fluxxor-state')
+if (oldData) {
+	Flux.deserialize(oldData)
+}
+else {
+	Flux.getActions('orders').createOrder({})
+}
+
+function saveState() {
+	localStorage.setItem('fluxxor-state', Flux.serialize())
+}
+
+Flux.getStore('orders').addListener('change', throttle(saveState, 500))
 
 React.render(<App flux={Flux} />, document.querySelector('.app'))
