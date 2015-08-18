593cd342e504752fddb7f84eadcb922c9a4c2533
Hawken Rives <hawkrives@gmail.com>
2015-08-17 21:14:15 -0500

    add {}

--- index.js
+++ index.js
@@ -11,11 +11,13 @@ import debounce from 'lodash/function/debounce'
 let Flux = new OrderFlux()
 
 const oldData = localStorage.getItem('fluxxor-state')
-if (oldData)
+if (oldData) {
 	Flux.deserialize(oldData)
+}
 
-if (!Flux.getStore('orders').state.orders || !Flux.getStore('orders').state.orders.size)
+if (!Flux.getStore('orders').state.orders || !Flux.getStore('orders').state.orders.size) {
 	Flux.getActions('orders').createOrder({})
+}
 
 let saveState = () => localStorage.setItem('fluxxor-state', Flux.serialize())
 Flux.getStore('orders').addListener('change', debounce(saveState, 1000))
