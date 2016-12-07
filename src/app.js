cf6aa4633d595d00d9a9cb60001169242800485e
Hawken Rives <hawkrives@gmail.com>
2016-12-07 09:41:58 -0600

    bloop

---
+++ src/app.js
@@ -0,0 +1,23 @@
+import React, {Component, PropTypes} from 'react'
+import OrderForm from './components/order-form'
+import FluxComponent from 'flummox/component'
+
+export default class App extends Component {
+	static propTypes = {
+		flux: PropTypes.object.isRequired,
+	}
+	render() {
+		return (
+			<div className='app'>
+				<header className='bmlogo'>
+					<img src='source/images/bmcheader.png' />
+				</header>
+				<FluxComponent
+					flux={this.props.flux}
+					connectToStores={['orders']}
+					render={storeState => <OrderForm order={storeState.orders.first()} actions={this.props.flux.getActions('orders')} />}
+				/>
+			</div>
+		)
+	}
+}
---
+++ src/index.js
@@ -0,0 +1,26 @@
+import intl from 'intl'
+
+import React from 'react'
+
+import App from './app'
+import OrderFlux from './flux/order-flux'
+
+import debounce from 'lodash/debounce'
+
+let Flux = new OrderFlux()
+
+const oldData = localStorage.getItem('fluxxor-state')
+if (oldData) {
+	Flux.deserialize(oldData)
+}
+
+if (!Flux.getStore('orders').state.orders || !Flux.getStore('orders').state.orders.size) {
+	Flux.getActions('orders').createOrder({})
+}
+
+let saveState = () => localStorage.setItem('fluxxor-state', Flux.serialize())
+Flux.getStore('orders').addListener('change', debounce(saveState, 1000))
+
+///
+React.render(<App flux={Flux} />, document.querySelector('.app'))
+///
